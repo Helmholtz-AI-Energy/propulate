beeceb0e422c51e2c365adc96c10135d0ae5bcfc
Oskar Taubert <oskar.taubert@kit.edu>
2020-04-20 15:00:45 +0200

    added another mutation operator for interval traits

--- src/propulate/propagators.py
+++ src/propulate/propagators.py
@@ -29,6 +29,7 @@ class Propagator():
             raise ValueError("Propagator has to sire more than 0 offspring.")
         return
     def __call__(self, inds):
+        raise NotImplementedError()
         return
 
 
@@ -51,6 +52,7 @@ class Cascade(Propagator):
         return inds
 
 
+# TODO random number of points to mutate
 class PointMutation(Propagator):
     def __init__(self, limits, probability=1., points=1):
         super(PointMutation, self).__init__(1, 1, probability)
@@ -64,7 +66,7 @@ class PointMutation(Propagator):
         if random.random() < self.probability:
             ind = copy.deepcopy(ind)
             ind.loss = None
-            to_mutate = random.sample(ind.keys(), self.offspring)
+            to_mutate = random.sample(ind.keys(), self.points)
             for i in to_mutate:
                 if type(ind[i]) == int:
                     ind[i] = random.randrange(*self.limits[i])
@@ -72,6 +74,34 @@ class PointMutation(Propagator):
                     ind[i] = random.uniform(*self.limits[i])
                 elif type(ind[i]) == str:
                     ind[i] = random.choice(self.limits[i])
+
+        return ind
+
+
+class IntervalMutationNormal(Propagator):
+    def __init__(self, limits, sigma_factor=.1, probability=1., points=1):
+        super(IntervalMutationNormal, self).__init__(1, 1, probability)
+        self.points = points
+        self.limits = limits
+        self.sigma_factor = sigma_factor
+        n_interval_traits = len([x for x in limits if type(limits[x][0]) == float])
+        if n_interval_traits < points:
+            raise ValueError("Too many points to mutate for individual with {} interval traits".format(n_interval_traits))
+        return
+
+    def __call__(self, ind):
+        if random.random() < self.probability:
+            ind = copy.deepcopy(ind)
+            ind.loss = None
+            interval_keys = [x for x in ind.keys() if type(ind[x])==float]
+            to_mutate = random.sample(interval_keys, self.points)
+            for i in to_mutate:
+                min_val, max_val = self.limits[i]
+                mu = ind[i]
+                sigma = (max_val-min_val)*self.sigma_factor
+                ind[i] = random.gauss(mu, sigma)
+                ind[i] = min(max_val, ind[i])
+                ind[i] = max(min_val, ind[i])
 
         return ind
 
@@ -113,20 +143,28 @@ class SelectUniform(Propagator):
         return random.sample(inds, self.offspring)
 
 
+# TODO children != 1 case
 class InitUniform(Propagator):
-    def __init__(self, limits):
-        super(InitUniform, self).__init__(0, 1, 1.)
+    def __init__(self, limits, parents=0, probability=1.):
+        """
+        In case of parents > 0 and probability < 1., call returns input individual without change
+        """
+        super(InitUniform, self).__init__(parents, 1, probability)
         self.limits = limits
         return
-    def __call__(self):
-        ind = Individual()
-        for limit in self.limits:
-            if type(self.limits[limit][0]) == int:
-                ind[limit] = random.randrange(*self.limits[limit])
-            elif type(self.limits[limit][0]) == float:
-                ind[limit] = random.uniform(*self.limits[limit])
-            elif type(self.limits[limit][0]) == str:
-                ind[limit] = random.choice(self.limits[limit])
-            else:
-                raise ValueError("Unknown type of limits. Has to be float for interval, int for ordinal, or string for categorical.")
-        return ind
+    def __call__(self, *inds):
+        if random.random() < self.probability:
+            ind = Individual()
+            for limit in self.limits:
+                if type(self.limits[limit][0]) == int:
+                    ind[limit] = random.randrange(*self.limits[limit])
+                elif type(self.limits[limit][0]) == float:
+                    ind[limit] = random.uniform(*self.limits[limit])
+                elif type(self.limits[limit][0]) == str:
+                    ind[limit] = random.choice(self.limits[limit])
+                else:
+                    raise ValueError("Unknown type of limits. Has to be float for interval, int for ordinal, or string for categorical.")
+            return ind
+        else :
+            ind = inds[0]
+            return ind
--- src/propulate/utils.py
+++ src/propulate/utils.py
@@ -1,12 +1,14 @@
-from .propagators import Cascade, PointMutation, MateUniform, SelectBest, SelectUniform, InitUniform
+from .propagators import Cascade, PointMutation, MateUniform, SelectBest, SelectUniform, InitUniform, IntervalMutationNormal
 
-def get_default_propagator(pop_size, limits, mate_prob, mut_prob):
+def get_default_propagator(pop_size, limits, mate_prob, mut_prob, random_prob):
     propagator = Cascade(
             [
                 SelectBest(pop_size),
                 SelectUniform(2),
                 MateUniform(mate_prob),
                 PointMutation(limits, mut_prob),
+                IntervalMutationNormal(limits, probability=1),
+                InitUniform(limits, parents=1, probability=random_prob),
             ]
         )
 
