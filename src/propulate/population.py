000fc52a9cd7925130dc2fd2f1bc02f1d7484bdf
Oskar Taubert <oskar.taubert@kit.edu>
2020-04-20 13:48:35 +0200

    updated comments

--- src/propulate/population.py
+++ src/propulate/population.py
@@ -1,4 +1,5 @@
 # TODO multi objective optimization?
+# TODO invalidate loss, when entry is modified so this does not have to be done by the propagator
 class Individual(dict):
     def __init__(self, generation=None, rank=None, traits=[]):
         super(Individual, self).__init__(list())
