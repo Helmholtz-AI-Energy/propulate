0b47c789bb2d59e9e3c2b7b53f43d38b059e9824
mcw92 <marie.weiel@kit.edu>
2022-03-31 16:00:24 +0200

    removed PyScaffold stuff

--- setup.py
+++ setup.py
@@ -2,10 +2,6 @@
 """
     Setup file for propulate.
     Use setup.cfg to configure your project.
-
-    This file was generated with PyScaffold 3.2.3.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
 """
 import sys
 
@@ -20,4 +16,4 @@ except VersionConflict:
 
 
 if __name__ == "__main__":
-    setup(use_pyscaffold=True)
+    setup()
