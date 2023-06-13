797ae1666ba02c87a511bd95db326f751ef5ea3d
Morridin <paulza@gmx.de>
2023-06-13 13:24:57 +0200

    Updated project/module contents.

---
+++ ap_pso/__init__.py
@@ -0,0 +1,7 @@
+"""
+This file is used to steer the visibility of classes within ap-pso project
+"""
+from optimizer import Optimizer
+from utils import get_default_propagator
+
+__all__ = ["Optimizer", "propagators", "get_default_propagator"]
