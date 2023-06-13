57badc37f0e2623f8e5258ba1145d4b2b8fdad74
Morridin <paulza@gmx.de>
2023-06-13 13:27:14 +0200

    Added a dummy function for get_default_propagator(...)

---
+++ ap_pso/utils.py
@@ -0,0 +1,30 @@
+"""
+This file contains some random util functions, as, for example, get_default_propagator
+"""
+from random import Random
+
+
+def get_default_propagator(pop_size: int, limits: dict, mate_prob: float, mut_prob: float, random_prob: float,
+                           sigma_factor: float = 0.05, rng: Random = None):
+    """
+    Returns a generic, but working propagator to use on Swarm objects in order to update the particles.
+
+
+    Parameters
+    ----------
+    pop_size : int
+               number of individuals in breeding population
+    limits : dict
+    mate_prob : float
+                uniform-crossover probability
+    mut_prob : float
+               point-mutation probability
+    random_prob : float
+                  random-initialization probability
+    sigma_factor : float
+                   scaling factor for obtaining std from search-space boundaries for interval mutation
+    rng : random.Random()
+          random number generator
+    """
+    _ = (pop_size, limits, mate_prob, mut_prob, random_prob, sigma_factor, rng)
+    pass
