6a50c3be831e7980f2dbe51d266e4c7b9040648d
Morridin <paulza@gmx.de>
2023-06-13 13:25:56 +0200

    Added Propagators to the implementation.
    Currently they don't do anything but existing.

---
+++ ap_pso/propagators/__init__.py
@@ -0,0 +1,9 @@
+"""
+This package holds all Propagator subclasses including the Propagator itself.
+"""
+
+__all__ = ["Propagator", "SelectMin", "SelectMax"]
+
+from propulate.propulate.propagators import SelectMin, SelectMax
+
+from propagator import Propagator
---
+++ ap_pso/propagators/propagator.py
@@ -0,0 +1,44 @@
+"""
+This file contains the 'abstract base class' for all propagators of this project.
+"""
+from random import Random
+
+from particle import Particle
+
+
+class Propagator:
+    """
+    Abstract base class for all propagators, i.e., evolutionary operators, in Propulate.
+
+    Take a collection of individuals and use them to breed a new collection of individuals.
+    """
+
+    def __init__(self, parents: int = 0, offspring: int = 0, rng: Random = None):
+        """
+        Constructor of Propagator class.
+
+        Parameters
+        ----------
+        parents : int
+                  number of input individuals (-1 for any)
+        offspring : int
+                    number of output individuals
+        rng : random.Random()
+              random number generator
+        """
+        self.parents = parents
+        self.rng = rng
+        self.offspring = offspring
+        if offspring == 0:
+            raise ValueError("Propagator has to sire more than 0 offspring.")
+
+    def __call__(self, particles: list[Particle]):
+        """
+        Apply propagator (not implemented!).
+
+        Parameters
+        ----------
+        particles: propulate.population.Individual
+              individuals the propagator is applied to
+        """
+        raise NotImplementedError()
