fca0f207e1d6083a84fe9c2013fab97438e0fd21
Morridin <paulza@gmx.de>
2023-06-13 13:23:30 +0200

    Added some docstring. Moved the Islands equivalent away.

---
+++ ap_pso/swarm.py
@@ -0,0 +1,16 @@
+"""
+This file contains the Swarm class, the technical equivalent to the Islands class of Propulate.
+"""
+
+
+class Swarm:
+    """
+    This class resembles together with the propagators the user program interface of this project.
+
+    Also, the Swarm is the instance, in which Particles are grouped together in order to optimize them.
+    """
+
+    def __init__(self):
+        pass
+
+
