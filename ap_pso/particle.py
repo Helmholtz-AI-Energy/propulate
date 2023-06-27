3e77a89f9320f6315c62d7dd17e2ab620e2f327f
Morridin <paulza@gmx.de>
2023-06-27 14:08:03 +0200

    Brought Swarm class to something I consider quite a good state.

--- ap_pso/particle.py
+++ ap_pso/particle.py
@@ -3,101 +3,24 @@ from typing import Callable
 import numpy as np
 from numpy import ndarray
 
+from ap_pso.utils import ExtendedPosition
+
 
 class Particle:
     """
     This class resembles a single particle within the particle swarm solving tasks.
     """
 
-    def __init__(self, dimension: tuple[int], target_fn: Callable, position: np.ndarray = None,
-                 velocity: np.ndarray = None, seed: int = None):
-        """
-        Constructor of Particle class.
+    def __init__(self, position: np.ndarray, velocity: np.ndarray):
+        assert position.shape == velocity.shape
 
-        Parameters
-        ----------
-        dimension : tuple[int]
-                    A tuple containing information on the dimensionality of the search space.
-                    All values that have something to do with the search space, are tested against this value
-                    to ensure usability. Only values > 0 are allowed.
-        target_fn : callable[np.ndarray -> double]
-                    The function given by this parameter is evaluated in each step of the algorithm
-        position :  optional np.ndarray of shape `dimension`
-                    The initial position of this Particle. Also, the initial value for p_best.
-        velocity :  optional np.ndarray of shape `dimension`
-                    The initial velocity of this Particle.
-        seed :      optional int
-                    The random number generator seed. If set, all values of position or velocity that are not set, will
-                    be filled with random numbers generated via the given seed.
-        """
-        assert all((x > 0 for x in dimension))
-        assert all((position is None or position.shape == dimension, velocity is None or velocity.shape == dimension))
+        self.position = position
+        self.velocity = velocity
+        self.loss: float = None
+        self.p_best: ExtendedPosition = None
 
-        self._search_space_dim = dimension
-        self._target_fn = target_fn
-
-        self._rng = None
-        if seed is not None:
-            self._rng = np.random.default_rng(seed)
-
-        self._position = np.zeros(self._search_space_dim)
-        if position is not None:
-            self._position = position
-        elif self._rng is not None:
-            self._position = np.array(self._rng.random(self._search_space_dim))
-
-        self._velocity = np.zeros(self._search_space_dim)
-        if velocity is not None:
-            self._velocity = velocity
-        elif self._rng is not None:
-            self._velocity = np.array(self._rng.random(self._search_space_dim))
-
-        self._p_best = self._g_best = self._position
-
-    def update(self, w_k: float, c_1: float, c_2: float, r_1: float, r_2: float) -> tuple[ndarray, ndarray]:
-        """
-        This method calculates the position and velocity of the particle for the next time step and returns them.
-        :param w_k: particle inertia - how strong influences the current v the speed for the particle in next round
-        :param c_1: cognitive factor - how good is the particle's brain
-        :param c_2: social factor - how strong the particle believes in the swarm
-        :param r_1: random factor to c_1
-        :param r_2: random factor to c_2
-        :return:
-        """
-        x: np.ndarray = self._position + self._velocity
-        v: np.ndarray = w_k * self._velocity + c_1 * r_1 * (self._p_best - self._position) + c_2 * r_2 * (
-                self._g_best - self._position)
-        return x, v
-
-    def eval(self) -> float:
-        """
-        Returns the value of the particles target function on the particle's current position.
-        """
-        return self._target_fn(self._position)
-
-    @property
-    def position(self) -> np.ndarray:
-        return self._position
-
-    @position.setter
-    def position(self, value: np.ndarray) -> None:
-        assert value.shape == self._search_space_dim
-        self._position = value
-
-    @property
-    def velocity(self) -> np.ndarray:
-        return self._velocity
-
-    @velocity.setter
-    def velocity(self, value: np.ndarray) -> None:
-        assert value.shape == self._search_space_dim
-        self._velocity = value
-
-    @property
-    def g_best(self) -> np.ndarray:
-        return self._g_best
-
-    @g_best.setter
-    def g_best(self, value: np.ndarray) -> None:
-        assert value.shape == self._search_space_dim
-        self._g_best = value
+    def update_p_best(self) -> None:
+        if self.loss is None:
+            return
+        if self.p_best is None or self.loss < self.p_best.loss:
+            self.p_best = ExtendedPosition(self.position, self.loss)
--- ap_pso/swarm.py
+++ ap_pso/swarm.py
@@ -1,6 +1,8 @@
 """
 This file contains the Swarm class, the technical equivalent to the Islands class of Propulate.
 """
+from typing import Optional
+
 from mpi4py import MPI
 
 from ap_pso.particle import Particle
@@ -21,24 +23,36 @@ class Swarm:
     one swarm so that everybody is always up to date.
     """
 
-    def __init__(self, num_workers: int, rank: int, propagator: Propagator, communicator: MPI.Comm):
-        self.particles: list[Particle] = []
+    def __init__(self, propagator: Propagator, communicator: MPI.Comm):
+        """
+        Swarm constructor.
+
+        :param propagator: The propagator to be used to update and evaluate the particles within this swarm
+        :param communicator: The communicator for this swarm's internal communication.
+        """
         self.propagator: Propagator = propagator
-        self.swarm_best: ExtendedPosition = None
-        self.archive: list[list[Particle]] = [] * num_workers
-        self.rank: int = rank
         self.communicator = communicator
-        self.size = num_workers
+
+        self.rank: int = communicator.rank
+        self.size: int = communicator.size
+
+        self.particles: list[Particle] = []
+        self.archive: list[list[Particle]] = [] * self.size
+
+        self.swarm_best: Optional[ExtendedPosition] = None
 
         # Create "randomly" initialised particles!
 
         # Communicate!
 
-    def update(self):
+    def update(self) -> None:
         """
-        This function runs an update on the worker's particle.
+        This method runs an update on the particles of the swarm. However, only the particle matching to the worker,
+        on which the swarm object is lying, is updated (usually, multiple similar swarm objects exist at the same time
+        within one communicator, all resembling the same swarm. This is a side effect of playing with MPI).
 
         The update is performed by the following steps:
+
         - First, a new particle with the stats of the old one plus one movement step update is created.
         - Then, the old particle is moved into archive.
         - In place of the old particle then the newly created is put.
@@ -49,7 +63,12 @@ class Swarm:
         self.archive[self.rank].append(old_p)
         self.particles[self.rank] = new_p
 
-    def _communicate_update(self):
+    def _communicate_update(self) -> None:
+        """
+        Private method to handle the swarm's internal updating communication.
+
+        At the same time, it performs updates on the swarm-global best value.
+        """
         for i in range(self.size):
             if i != self.rank:
                 self.communicator.send(self.particles[self.rank], i, TELL_TAG)
@@ -59,10 +78,25 @@ class Swarm:
             if not self.communicator.iprobe(tag=TELL_TAG, status=status):
                 break
             source = status.Get_source()
-            incoming_p = self.communicator.recv(source=source, tag=TELL_TAG)
+            incoming_p: Particle = self.communicator.recv(source=source, tag=TELL_TAG)
             self.archive[source].append(self.particles[source])
             self.particles[source] = incoming_p
+            if incoming_p.p_best is not None and (self.swarm_best is None or incoming_p.p_best < self.swarm_best):
+                self.swarm_best = incoming_p.p_best
 
+    def evaluate_particle(self) -> None:
+        """
+        This method calls the swarm's propagator's loss function
+        on the particles and thus evaluates their fitness.
 
+        The method always only considers the particle matching the rank of
+        the worker within the swarm's internal communicator.
 
-
+        After evaluation, the method also starts a communication round in
+        order to update the rest of the workers on the swarm.
+        """
+        p: Particle = self.particles[self.rank]
+        p.loss = self.propagator.loss_fn(p.position)
+        p.update_p_best()
+        self.particles[self.rank] = p
+        self._communicate_update()
