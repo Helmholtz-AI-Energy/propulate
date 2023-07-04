b847ea6001a2143b3f139d5bc2b5693b444fe14d
Morridin <paulza@gmx.de>
2023-07-04 15:20:47 +0200

    Reset project to be able to integrate the propulate fork

--- ap_pso/propagators/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-This package holds all Propagator subclasses including the Propagator itself.
-"""
-
-__all__ = ["Propagator", "SelectMin", "SelectMax"]
-
-from propulate.propulate.propagators import SelectMin, SelectMax
-
-from propagator import Propagator
