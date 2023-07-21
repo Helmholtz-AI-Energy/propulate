7c0fe0b43fbcc8811a413695731d2377c41dbb3c
Marie Weiel <marie.weiel@kit.edu>
2023-07-21 12:05:43 +0200

    added mocking for mpi4py

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,10 @@ author = 'Oskar Taubert, Marie Weiel'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+            'sphinx.ext.autodoc',
 ]
+
+autodoc_mock_imports = ['mpi4py']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
