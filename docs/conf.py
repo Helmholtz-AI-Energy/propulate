e82ed52d1987b88d9a5500f758b4389a68e8e707
Marie Weiel <marie.weiel@kit.edu>
2023-07-21 12:05:43 +0200

    added mocking for mpi4py

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,9 @@ extensions = [
 
 autoapi_dirs = ["../propulate"]
 autoapi_python_class_content = "both"
+
+autodoc_mock_imports = ['mpi4py']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
