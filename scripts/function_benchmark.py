1b383323cef27c0a02576f2ee3b7a35b5781b183
Marie Weiel <marieweiel@dyn-dhcp-172-22-83-193.scc.kit.edu>
2023-08-10 14:38:52 +0200

    fixed bug in function implementations

--- scripts/function_benchmark.py
+++ scripts/function_benchmark.py
@@ -25,7 +25,7 @@ def bukin_n6(params: Dict[str, float]) -> float:
 
 def egg_crate(params: Dict[str, float]) -> float:
     """
-    Egg-crate: continuous, non-convex, separable, differentiable, multimodal
+    Egg-crate function: continuous, non-convex, separable, differentiable, multimodal
 
     Input domain: -5 <= x, y <= 5
     Global minimum -1 at (x, y) = (0, 0)
@@ -46,7 +46,7 @@ def egg_crate(params: Dict[str, float]) -> float:
 
 def himmelblau(params: Dict[str, float]) -> float:
     """
-    Himmelblau: continuous, non-convex, non-separable, differentiable, multimodal
+    Himmelblau function: continuous, non-convex, non-separable, differentiable, multimodal
 
     Input domain: -6 <= x, y <= 6
     Global minimum 0 at (x, y) = (3, 2)
@@ -67,7 +67,7 @@ def himmelblau(params: Dict[str, float]) -> float:
 
 def keane(params: Dict[str, float]) -> float:
     """
-    Keane: continuous, non-convex, non-separable, differentiable, multimodal
+    Keane function: continuous, non-convex, non-separable, differentiable, multimodal
 
     Input domain: -10 <= x, y <= 10
     Global minimum 0.6736675 at (x, y) = (1.3932491, 0) and (x, y) = (0, 1.3932491)
@@ -88,7 +88,7 @@ def keane(params: Dict[str, float]) -> float:
 
 def leon(params: Dict[str, float]) -> float:
     """
-    Leon: continuous, non-convex, non-separable, differentiable, non-multimodal, non-random, non-parametric
+    Leon function: continuous, non-convex, non-separable, differentiable, non-multimodal, non-random, non-parametric
 
     Input domain: 0 <= x, y <= 10
     Global minimum 0 at (x, y) =(1, 1)
@@ -109,7 +109,7 @@ def leon(params: Dict[str, float]) -> float:
 
 def rastrigin(params: Dict[str, float]) -> float:
     """
-    Rastrigin: continuous, non-convex, separable, differentiable, multimodal
+    Rastrigin function: continuous, non-convex, separable, differentiable, multimodal
 
     Input domain: -5.12 <= x, y <= 5.12
     Global minimum -20 at (x, y) = (0, 0)
@@ -130,7 +130,7 @@ def rastrigin(params: Dict[str, float]) -> float:
 
 def schwefel(params: Dict[str, float]) -> float:
     """
-    Schwefel 2.20: continuous, convex, separable, non-differentiable, non-multimodal
+    Schwefel 2.20 function: continuous, convex, separable, non-differentiable, non-multimodal
 
     Input domain: -100 <= x, y <= 100
     Global minimum 0 at (x, y) = (0, 0)
@@ -144,9 +144,8 @@ def schwefel(params: Dict[str, float]) -> float:
     -------
     float: function value
     """
-    x = params["x"]
-    y = params["y"]
-    return np.abs(x) + np.abs(y)
+    params = np.array(list(params.values()))
+    return len(params) * 418.982887 - np.sum(params * np.sin(np.sqrt(np.abs(params))))
 
 
 def sphere(params: Dict[str, float]) -> float:
@@ -163,9 +162,27 @@ def sphere(params: Dict[str, float]) -> float:
     -------
     float : function value
     """
-    x = params["x"]
-    y = params["y"]
-    return x ** 2 + y ** 2
+    return np.sum(np.array(list(params.values())) ** 2)
+
+
+def step(params: Dict[str, float]) -> float:
+    """
+    Step function
+
+    Input domain: -5 <= (x_i) <= 5
+    Global minimum 0 at (x_i) = -5, i = 1,...,N
+
+    Params
+    ------
+    params: dict
+            function parameters
+
+    Returns
+    -------
+    float: function value
+    """
+    params = np.array(list(params.values()))
+    return np.sum(np.floor(params))
 
 
 def get_function_search_space(fname: str) -> (Callable, Dict[str, Tuple[float, float]]):
@@ -221,14 +238,20 @@ def get_function_search_space(fname: str) -> (Callable, Dict[str, Tuple[float, f
     elif fname == "schwefel":
         function = schwefel
         limits = {
-            "x": (-100.0, 100.0),
-            "y": (-100.0, 100.0),
+            "x": (-500.0, 500.0),
+            "y": (-500.0, 500.0),
         }
     elif fname == "sphere":
         function = sphere
         limits = {
             "x": (-5.12, 5.12),
             "y": (-5.12, 5.12),
+        }
+    elif fname == "step":
+        function = step
+        limits = {
+            "x": (-5., 5.),
+            "y": (-5., 5.),
         }
     else:
         ValueError(f"Function {fname} undefined...exiting")
