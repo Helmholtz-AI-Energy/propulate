8f349fe253eb25eb482081f8b3d1a905c6cbc5ba
Marie Weiel <marie.weiel@kit.edu>
2023-08-11 15:15:40 +0200

    enabled Sphinx auto-api use

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'propulate'
-copyright = '2023, Oskar Taubert, Marie Weiel, Helmholtz AI'
-author = 'Oskar Taubert, Marie Weiel'
+project = "propulate"
+copyright = "2023, Oskar Taubert, Marie Weiel, Helmholtz AI"
+author = "Oskar Taubert, Marie Weiel"
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,18 +28,20 @@ author = 'Oskar Taubert, Marie Weiel'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-            'sphinx.ext.autodoc',
+    "sphinx.ext.autodoc",
+    "autoapi.extension",
+    "sphinx.ext.napoleon"
 ]
 
-autodoc_mock_imports = ['mpi4py']
-
+autoapi_dirs = ["../propulate"]
+autoapi_python_class_content = "both"
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,9 +49,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
