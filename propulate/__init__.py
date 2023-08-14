4972fb88826b6487214c5623ab2009c5321e56ac
Marie Weiel <marie.weiel@kit.edu>
2023-08-14 13:36:07 +0200

    harmonized quotation marks

--- propulate/__init__.py
+++ propulate/__init__.py
@@ -6,7 +6,7 @@ try:
     dist_name = __name__
     __version__ = get_distribution(dist_name).version
 except DistributionNotFound:
-    __version__ = 'unknown'
+    __version__ = "unknown"
 finally:
     del get_distribution, DistributionNotFound
 
@@ -14,4 +14,4 @@ from .islands import Islands
 from .propulator import Propulator
 from .pollinator import Pollinator
 
-__all__ = ['Islands', 'Propulator', 'Pollinator']
+__all__ = ["Islands", "Propulator", "Pollinator"]
