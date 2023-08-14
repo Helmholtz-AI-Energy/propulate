381ec53c62ffb225170af3557fb6e2d50b01b2c1
Morridin <paulza@gmx.de>
2023-08-14 15:14:03 +0200

    Updated pso test scripts.

--- ap_pso/scripts/pso_example.py
+++ ap_pso/scripts/pso_example.py
@@ -5,35 +5,21 @@ import sys
 from mpi4py import MPI
 
 from ap_pso.propagators import PSOInitUniform, VelocityClampingPropagator, ConstrictionPropagator, PSOCompose, \
-    BasicPSOPropagator, StatelessPSOPropagator
+    BasicPSOPropagator, StatelessPSOPropagator, CanonicalPropagator
 from propulate import Islands
 from propulate.propagators import Conditional, InitUniform
+from scripts.function_benchmark import get_function_search_space
 
 ############
 # SETTINGS #
 ############
 
-fname = sys.argv[1]  # Get function to optimize from command-line.
+function_name = sys.argv[1]  # Get function to optimize from command-line.
 NUM_GENERATIONS: int = int(sys.argv[2])  # Set number of generations.
 POP_SIZE = 2 * MPI.COMM_WORLD.size  # Set size of breeding population.
 num_migrants = 1
 
-
-# SPHERE
-# continuous, convex, separable, non-differentiable, non-multimodal
-# input domain: -5.12 <= x, y <= 5.12
-# global minimum 0 at (x, y) = (0, 0)
-def sphere(params):
-    x = params["x"]
-    y = params["y"]
-    return x ** 2 + y ** 2
-
-
-if fname == "sphere":
-    function = sphere
-    limits = {"x": (-5.12, 5.12), "y": (-5.12, 5.12), }
-else:
-    sys.exit("ERROR: Function undefined...exiting")
+function, limits = get_function_search_space(function_name)
 
 if __name__ == "__main__":
     # migration_topology = num_migrants*np.ones((4, 4), dtype=int)
@@ -44,8 +30,10 @@ if __name__ == "__main__":
     propagator = PSOCompose(
         [
             # VelocityClampingPropagator(0.7298, 1.49618, 1.49618, MPI.COMM_WORLD.rank, limits, rng, 0.6)
-            ConstrictionPropagator(2.49618, 2.49618, MPI.COMM_WORLD.rank, limits, rng)
-            # BasicPSOPropagator(0.7298,1.49618,1.49618, MPI.COMM_WORLD.rank, limits, rng)
+            # VelocityClampingPropagator(0.7298, 0.5, 0.5, MPI.COMM_WORLD.rank, limits, rng, 0.6)
+            # ConstrictionPropagator(2.49618, 2.49618, MPI.COMM_WORLD.rank, limits, rng)
+            # BasicPSOPropagator(0.7298, 0.5, 0.5, MPI.COMM_WORLD.rank, limits, rng)
+            CanonicalPropagator(2.49618, 2.49618, MPI.COMM_WORLD.rank, limits, rng)
             # StatelessPSOPropagator(0, 1.49618, 1.49618, MPI.COMM_WORLD.rank, limits, rng) # Attention! Does not work with current chart drawing script!
         ]
     )
@@ -54,5 +42,6 @@ if __name__ == "__main__":
     propagator = Conditional(POP_SIZE, propagator, init)
 
     islands = Islands(function, propagator, rng, generations=NUM_GENERATIONS, checkpoint_path='./checkpoints/',
-                      migration_probability=0)
-    islands.evolve(top_n=1, logging_interval=1, DEBUG=2)
+                      migration_probability=0, pollination=False)
+    islands.evolve(top_n=1, logging_interval=1)
+    islands.propulator.paint_graphs(function_name)
