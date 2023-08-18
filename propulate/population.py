951fffa2bbe221f8d190da83dd65342dca7bd4b1
Marie Weiel <marie.weiel@kit.edu>
2023-08-18 09:07:27 +0200

    adapted docstring

--- propulate/population.py
+++ propulate/population.py
@@ -2,13 +2,16 @@ from decimal import Decimal
 
 
 class Individual(dict):
+    """
+    An individual represents a candidate solution to the considered optimization problem.
+    """
     def __init__(
             self,
             generation: int = -1,
             rank: int = -1
     ) -> None:
         """
-        Initialize individual with given parameters.
+        Initialize an individual with given parameters.
 
         Parameters
         ----------
@@ -18,9 +21,9 @@ class Individual(dict):
               rank (-1 if unset)
         """
         super(Individual, self).__init__(list())
-        self.generation = generation  # Equals each worker's iteration for continuous population in propulate.
+        self.generation = generation  # Equals each worker's iteration for continuous population in Propulate.
         self.rank = rank
-        self.loss = None  # NOTE set to None instead of inf since there are no comparisons
+        self.loss = None  # Set to None instead of inf since there are no comparisons
         self.active = True
         self.island = -1  # island of origin
         self.current = -1  # current responsible worker
@@ -30,13 +33,15 @@ class Individual(dict):
         self.evalperiod = None  # evaluation duration
 
     def __repr__(self) -> str:
+        """
+        String representation of an ``Individual`` instance.
+        """
         rep = {
             key: (
                 f"{Decimal(self[key]):.2E}" if type(self[key]) == float else self[key]
             )
             for key in self
         }
-        is_active = "active" if self.active else "deactivated"
         if self.loss is None:
             loss_str = f"{self.loss}"
         else:
@@ -51,7 +56,7 @@ class Individual(dict):
             other
     ) -> bool:
         """
-        Define equality operator "==" for class Individual`.
+        Define equality operator ``==`` for class ``Individual``.
 
         Checks for equality of traits, loss, generation, worker rank, birth island, and active status. Other attributes,
         like migration steps, are not considered.
@@ -69,7 +74,7 @@ class Individual(dict):
         Raises
         ------
         TypeError
-            If other is not an instance or subclass of `Individual`.
+            If other is not an instance or subclass of ``Individual``.
         """
         # Check if object to compare to is of the same class.
         if not isinstance(other, self.__class__):
@@ -98,7 +103,7 @@ class Individual(dict):
             other
     ) -> bool:
         """
-        Define alternative equality check for class `Individual`.
+        Define alternative equality check for class ``Individual``.
 
         Checks for equality of traits and loss. Other attributes, like birth island or generation, are not considered.
 
@@ -115,7 +120,7 @@ class Individual(dict):
         Raises
         ------
         TypeError
-            If other is not an instance or subclass of `Individual`.
+            If other is not an instance or subclass of ``Individual``.
         """
         # Check if object to compare to is of the same class.
         if not isinstance(other, self.__class__):
