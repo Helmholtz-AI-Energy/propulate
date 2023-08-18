6a3fa761bb104e250a0f6afb482ae683a3fc732d
DonnyDev <jomny11@googlemail.com>
2023-08-18 17:17:56 +0200

    updated script

--- scripts/cma_es_benchmark.py
+++ scripts/cma_es_benchmark.py
@@ -8,7 +8,7 @@ from mpi4py import MPI
 
 import numpy as np
 from propulate import Islands
-from propulate.propagators import SelectMin, SelectMax, CMAPropagator, BasicCMA, ActiveCMA
+from propulate.propagators import SelectMin, SelectMax, CMAPropagator, BasicCMA, ActiveCMA, Propagator
 from propulate.utils import get_default_propagator
 from propulate.population import Individual
 
@@ -285,71 +285,113 @@ def get_function_search_space(fname, problem_dimension):
     return function, limits
 
 
+def get_propagator(prop_id: int, limits: Dict) -> Propagator:
+    propagator = None
+
+    if prop_id == 0:
+        propagator = get_default_propagator(
+            pop_size=4,
+            limits=limits,
+            mate_prob=0.7,
+            mut_prob=0.4,
+            random_prob=0.1,
+            rng=rng
+        )
+    elif prop_id == 1:
+        propagator = CMAPropagator(BasicCMA(), limits, rng, exploration=args.exploration,
+                                   select_worst_all_time=args.select_worst_all_time,
+                                   pop_size=args.pop_size, pool_size=args.pool_size)
+    elif prop_id == 2:
+        propagator = CMAPropagator(ActiveCMA(), limits, rng, exploration=args.exploration,
+                                   select_worst_all_time=args.select_worst_all_time,
+                                   pop_size=args.pop_size, pool_size=args.pool_size)
+    return propagator
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Benchmark CMA-ES in Propulate")
-    parser.add_argument("--function", type=str, default="sphere", help="The function to optimize.")
+
+    # parser.add_argument("--function", type=str, default="sphere", help="The function to optimize.")
     parser.add_argument("--generation", type=int, default=100, help="The number of generations in the algorithm.")
     parser.add_argument("--pop_size", type=int, default=None, help="The size of the breeding population.")
-    parser.add_argument("--num_isles", type=int, default=2, help="The number of isles for the Island model.")
+    parser.add_argument("--num_islands", type=int, default=2, help="The number of isles for the Island model.")
     parser.add_argument("--migration_probability", type=float, default=0.9, help="The probability of migration for each generation.")
-    parser.add_argument("--pollination", type=bool, default=False, help="Whether to use real migration or pollination.")
-    parser.add_argument("--default_propulate", type=bool, default=False, help="Whether to use the default propulate propagator.")
-    parser.add_argument("--active_cma", type=bool, default=False, help="Whether to use active CMA or the basic algorithm.")
     parser.add_argument("--exploration", type=bool, default=False, help="Whether to update the covariance matrix after each generation")
     parser.add_argument("--select_worst_all_time", type=bool, default=False, help="Whether to always use the worst individuals of all time in the case of active cma.")
     parser.add_argument("--dimension", type=int, default=2, help="The number of dimension in the search space.")
     parser.add_argument("--checkpoint_path", type=str, default='./checkpoint_data/', help="The path for the checkpoint and logging data.")
-    parser.add_argument("--pool_size", type=int, default=2, help="The pool size of the cma propagator.")
+    parser.add_argument("--pool_size", type=int, default=3, help="The pool size of the cma propagator.")
 
     args = parser.parse_args()
 
+    functions = {
+        "rosenbrock",
+        "step",
+        "quartic",
+        "rastrigin",
+        "griewank",
+        "schwefel",
+        "sphere",
+        "bisphere",
+        "birastrigin"
+    }
+
     # Set up migration topology.
     num_migrants = 1  # Set number of individuals migrating at once.
-    migration_topology = num_migrants * np.ones(  # Set up fully connected migration topology.
-        (args.num_isles, args.num_isles),
-        dtype=int
-    )
-    np.fill_diagonal(migration_topology, 0)  # An island does not send migrants to itself.
 
-    func, limits = get_function_search_space(
-        args.function, args.dimension)  # Get callable function and search-space limits from function name.
-    rng = random.Random(
-        MPI.COMM_WORLD.rank)  # Set up separate random number generator for evolutionary optimization process.
+    configurations = {
+        "default_prop_migr": {"checkpoint_path": "default_prop/migr", "num_islands": args.num_islands, "pollination": False, "propagator": 0, "dimension": args.dimension}, # TODO Wie num_islands setzen für default propulat?
+        "default_prop_polli": {"checkpoint_path": "default_prop/polli", "num_islands": args.num_islands, "pollination": True, "propagator": 0, "dimension": args.dimension}, # TODO Wie num_islands setzen für default propulate?
+        "cma_basic": {"checkpoint_path": "cma/basic", "num_islands": 1, "pollination": False, "propagator": 1, "dimension": args.dimension},
+        "cma_active": {"checkpoint_path": "cma/active", "num_islands": 1, "pollination": False, "propagator": 2, "dimension": args.dimension},
+        "cma_island_basic_migr": {"checkpoint_path": "cma_island/basic/migr", "num_islands": args.num_islands, "pollination": False, "propagator": 1, "dimension": args.dimension},
+        "cma_island_active_migr": {"checkpoint_path": "cma_island/active/migr", "num_islands": args.num_islands, "pollination": False, "propagator": 2, "dimension": args.dimension},
+        "multi_dim_default_prop_polli_10": {"checkpoint_path": "multi_dim/default_prop/polli", "num_islands": args.num_islands, "pollination": True, "propagator": 0, "dimension": 10},
+        "multi_dim_cma_active_10": {"checkpoint_path": "multi_dim/cma/active", "num_islands": 1, "pollination": False, "propagator": 2, "dimension": 10},
+        "multi_dim_cma_island_active_migr_10": {"checkpoint_path": "multi_dim/cma_island/active/migr", "num_islands": args.num_islands, "pollination": False, "propagator": 2, "dimension": 10}, # TODO auch mit basic?
+        "multi_dim_default_prop_polli_30": {"checkpoint_path": "multi_dim/default_prop/polli", "num_islands": args.num_islands, "pollination": True, "propagator": 0, "dimension": 30},
+        "multi_dim_cma_active_30": {"checkpoint_path": "multi_dim/cma/active", "num_islands": 1, "pollination": False, "propagator": 2, "dimension": 30},
+        "multi_dim_cma_island_active_migr_30": {"checkpoint_path": "multi_dim/cma_island/active/migr", "num_islands": args.num_islands, "pollination": False, "propagator": 2, "dimension": 30}  # TODO auch mit basic?
+    }
 
-    # Set up evolutionary operator.
-    if args.default_propulate:
-        propagator = get_default_propagator(                # Get default evolutionary operator.
-                pop_size=4,                          # Breeding population size TODO Andere pop size als cma
-                limits=limits,                              # Search-space limits
-                mate_prob=0.7,                              # Crossover probability
-                mut_prob=0.4,                               # Mutation probability
-                random_prob=0.1,                            # Random-initialization probability
-                rng=rng                                     # Random number generator
-            )
-    else:
-        adapter = ActiveCMA() if args.active_cma else BasicCMA()
-        propagator = CMAPropagator(adapter, limits, rng, exploration=args.exploration, select_worst_all_time=args.select_worst_all_time, pop_size=args.pop_size, pool_size=args.pool_size)
+    for f in functions:
+        for run in range(1, 2):
+            for config in configurations.values():
 
-    # Set up island model.
-    islands = Islands(
-        func,  # Function to optimize
-        propagator,  # Evolutionary operator
-        rng,  # Random number generator
-        generations=args.generation,  # Number of generations
-        num_islands=args.num_isles,  # Number of separate evolutionary islands
-        migration_topology=migration_topology,          # Migration topology
-        checkpoint_path=args.checkpoint_path,  # Path to potentially read checkpoints from and write new checkpoints to
-        migration_probability=args.migration_probability,  # Migration probability
-        emigration_propagator=SelectMin,  # Emigration propagator (how to select migrants)
-        immigration_propagator=SelectMax,
-        # Immigration propagator (only relevant for pollination, how to choose individuals to be replaced by immigrants)
-        pollination=args.pollination,  # Pollination or real migration?
-    )
+                MPI.COMM_WORLD.barrier()
+                rng = random.Random(
+                    MPI.COMM_WORLD.rank)  # Set up separate random number generator for evolutionary optimization process.
+                if config["num_islands"] > 1:
+                    migration_topology = num_migrants * np.ones(  # Set up fully connected migration topology.
+                        (config["num_islands"], config["num_islands"]),
+                        dtype=int
+                    )
+                    np.fill_diagonal(migration_topology, 0)  # An island does not send migrants to itself.
+                else:
+                    migration_topology = None
+                migration_probability = args.migration_probability if config["num_islands"] > 1 else 0
+                func, limits = get_function_search_space(f, config["dimension"])  # Get callable function and search-space limits from function name.
 
-    # Run actual optimization.
-    islands.evolve(
-        top_n=1,
-        # Top-n best individuals are returned and printed (whole population can be accessed from checkpoint file).
-        logging_interval=1,  # Logging interval used for print-outs.
-        debug=2  # Debug / verbosity level
-    )
+                MPI.COMM_WORLD.barrier()
+                islands = Islands(
+                    func,  # Function to optimize
+                    get_propagator(config["propagator"], limits),  # Evolutionary operator
+                    rng,  # Random number generator
+                    generations=args.generation,  # Number of generations
+                    num_islands=config["num_islands"],  # Number of separate evolutionary islands
+                    migration_topology=migration_topology,          # Migration topology
+                    checkpoint_path=f"benchmark/{config['checkpoint_path']}_{f}_run{run}",  # Path to potentially read checkpoints from and write new checkpoints to
+                    migration_probability=migration_probability,  # Migration probability
+                    emigration_propagator=SelectMin,  # Emigration propagator (how to select migrants)
+                    immigration_propagator=SelectMax,
+                    # Immigration propagator (only relevant for pollination, how to choose individuals to be replaced by immigrants)
+                    pollination=config["pollination"],  # Pollination or real migration?
+                )
+                MPI.COMM_WORLD.barrier()
+                # Run actual optimization.
+                islands.evolve(
+                    top_n=1,
+                    logging_interval=1,  # Logging interval used for print-outs.
+                    debug=2  # Debug / verbosity level
+                )
+
--- scripts/run_cma_benchmark.sh
+++ scripts/run_cma_benchmark.sh
@@ -2,10 +2,10 @@
 
 # TODO --cpu-bind=cores output-name srun?
 # TODO STORE TIMES ETC?
-# TODO CPUS CPUSPERNODE RAUS
+# TODO Bispehre
 
 # Use GNU getopt to parse customized command-line arguments
-CMA_COMMAND=$(getopt -o: --long generation:,pop_size:,num_isles:,migration_probability:,pool_size:,exploration:,select_worst_all_time:,dimension:,main_path:,directory:,nodes:,cpus: -n 'getopt_errors_cma' -- "$@")
+CMA_COMMAND=$(getopt -o: --long generation:,pop_size:,num_isles:,migration_probability:,pool_size:,exploration:,select_worst_all_time:,dimension:,main_path:,directory:,nodes: -n 'getopt_errors_cma' -- "$@")
 
 if [ $? != 0 ] ; then echo "Command-line error occurred. Terminating..." >&2 ; exit 1 ; fi
 
@@ -22,9 +22,8 @@ SELECT_WORST_ALL_TIME=false
 DIMENSION=2
 MAIN_PATH="cma_es_benchmark.py"
 DIRECTORY="benchmark"
-NODES=1
+NODES=4
 CPUS_PER_NODE=8
-#WORKERS=0
 
 # Iterate through parsed options
 while true; do
@@ -40,7 +39,6 @@ while true; do
     --main_path ) MAIN_PATH="$2"; shift 2 ;;
     --directory ) DIRECTORY="$2"; shift 2 ;;
     --nodes ) NODES="$2"; shift 2 ;;
-    --cpus ) CPUS_PER_NODE="$2"; shift 2 ;;
     -- ) shift; break ;;
     * ) break ;;
   esac
@@ -55,11 +53,9 @@ BASE_SCRIPT="#!/bin/bash
 
 #SBATCH --job-name=${JOB_NAME}
 #SBATCH --output=${JOB_NAME}.out
-#SBATCH --time=48:00:00
 #SBATCH --nodes=$NODES
 #SBATCH --ntasks=$NTASKS
-#SBATCH --gres=cpu:$CPUS_PER_NODE
-#SBATCH --partition=multiple
+#SBATCH --partition=dev_multiple
 
 module purge
 module load compiler/gnu/12.1
@@ -69,9 +65,14 @@ source propulate_venv/bin/activate
 
 srun python -u $MAIN_PATH \
 --generation $GENERATION \
+--num_isles $NUM_ISLES
 --pop_size $POP_SIZE \
---pool_size $pool_size \
+--pool_size $POOL_SIZE \
+--exploration $EXPLORATION \
+--select_worst_all_time $SELECT_WORST_ALL_TIME \
+--dimension $DIMENSION \
 --migration_probability $MIGRATION_PROBABILITY"
+
 
 # Check if the directory already exists
 if [ -d "$DIRECTORY" ]; then
@@ -91,75 +92,3 @@ exploration: ${EXPLORATION}
 select_worst_all_time: ${SELECT_WORST_ALL_TIME}
 dimension: ${DIMENSION}"
 
-# make directories
-mkdir -p "$DIRECTORY/default_prop/migr"
-mkdir -p "$DIRECTORY/default_prop/polli"
-mkdir -p "$DIRECTORY/cma/basic"
-mkdir -p "$DIRECTORY/cma/active"
-mkdir -p "$DIRECTORY/cma_island/active/migr"
-mkdir -p "$DIRECTORY/cma_island/basic/migr"
-#mkdir -p "$DIRECTORY/cma_island/active/polli"
-#mkdir -p "$DIRECTORY/cma_island/basic/polli"
-
-mkdir -p "$DIRECTORY/multi_dim/default_prop/polli"
-mkdir -p "$DIRECTORY/multi_dim/cma/active"
-mkdir -p "$DIRECTORY/multi_dim/cma_island/active/migr"
-
-echo "$PARAMETER_SUMMARY" > "$DIRECTORY/parameters.txt"
-
-# Script for each configuration
-FUNCTIONS=("sphere" "rosenbrock" "step" "quartic" "rastrigin" "griewank" "schwefel")
-
-PREFIX_SCRIPT_DEFAULT="$BASE_SCRIPT --dimension $DIMENSION --num_isles $NUM_ISLES --default_propulate true"
-PREFIX_SCRIPT_CMA="$BASE_SCRIPT --dimension $DIMENSION --num_isles 1 --default_propulate false --exploration $EXPLORATION --select_worst_all_time $SELECT_WORST_ALL_TIME"
-PREFIX_SCRIPT_CMA_ISLAND="$BASE_SCRIPT --dimension $DIMENSION --num_isles $NUM_ISLES --default_propulate false --exploration $EXPLORATION --select_worst_all_time $SELECT_WORST_ALL_TIME"
-
-PREFIX_SCRIPT_DEFAULT_MULTI="$BASE_SCRIPT --num_isles $NUM_ISLES --default_propulate true"
-PREFIX_SCRIPT_CMA_MULTI="$BASE_SCRIPT --num_isles 1 --default_propulate false --exploration $EXPLORATION --select_worst_all_time $SELECT_WORST_ALL_TIME"
-PREFIX_SCRIPT_CMA_ISLAND_MULTI="$BASE_SCRIPT --num_isles $NUM_ISLES --default_propulate false --exploration $EXPLORATION --select_worst_all_time $SELECT_WORST_ALL_TIME"
-
-for func in "${FUNCTIONS[@]}"; do
-  for ((run=1; run<=10; run++)); do
-    # Default Propulate Benchmark
-    echo "$PREFIX_SCRIPT_DEFAULT --function \"$func\" --pollination false" > "$DIRECTORY/default_prop/migr/default_prop_migr_${func}_run${run}.sh"
-    echo "$PREFIX_SCRIPT_DEFAULT --function \"$func\" --pollination true" > "$DIRECTORY/default_prop/polli/default_prop_polli_${func}_run${run}.sh"
-
-    # Basic CMA
-    echo "$PREFIX_SCRIPT_CMA --function \"$func\" --active_cma false" > "$DIRECTORY/cma/basic/cma_basic_${func}_run${run}.sh"
-    # Active CMA
-    echo "$PREFIX_SCRIPT_CMA --function \"$func\" --active_cma true" > "$DIRECTORY/cma/active/cma_active_${func}_run${run}.sh"
-
-    # CMA + Island
-    echo "$PREFIX_SCRIPT_CMA_ISLAND --function \"$func\" --active_cma false --pollination false" > "$DIRECTORY/cma_island/basic/migr/cma_basic_migr_${func}_run${run}.sh"
-    #echo "$PREFIX_SCRIPT_CMA_ISLAND --function \"$func\" --active_cma false --pollination true" > "$DIRECTORY/cma_island/basic/polli/cma_basic_polli_${func}_run${run}.sh"
-    echo "$PREFIX_SCRIPT_CMA_ISLAND --function \"$func\" --active_cma true --pollination false" > "$DIRECTORY/cma_island/active/migr/cma_active_migr_${func}_run${run}.sh"
-    #echo "$PREFIX_SCRIPT_CMA_ISLAND --function \"$func\" --active_cma true --pollination true" > "$DIRECTORY/cma_island/active/polli/cma_active_polli_${func}_run${run}.sh"
-
-    # Default Propulate Multi Dimensions
-    echo "$PREFIX_SCRIPT_DEFAULT_MULTI --dimension 10 --function \"$func\" --pollination true" > "$DIRECTORY/multi_dim/default_prop/polli/multi_10_default_prop_polli_${func}_run${run}.sh"
-    echo "$PREFIX_SCRIPT_DEFAULT_MULTI --dimension 100 --function \"$func\" --pollination true" > "$DIRECTORY/multi_dim/default_prop/polli/multi_100_default_prop_polli_${func}_run${run}.sh"
-    # CMA Active Multi Dimensions
-    echo "$PREFIX_SCRIPT_CMA_MULTI --dimension 10 --function \"$func\" --active_cma true" > "$DIRECTORY/multi_dim/cma/active/multi10_cma_active_${func}_run${run}.sh"
-    echo "$PREFIX_SCRIPT_CMA_MULTI --dimension 100 --function \"$func\" --active_cma true" > "$DIRECTORY/multi_dim/cma/active/multi100_cma_active_${func}_run${run}.sh"
-    # CMA Active + Island Multi Dimensions
-    echo "$PREFIX_SCRIPT_CMA_ISLAND_MULTI --dimension 10 --function \"$func\" --active_cma true --pollination false" > "$DIRECTORY/multi_dim/cma_island/active/migr/multi10_cma_active_migr_${func}_run${run}.sh"
-    echo "$PREFIX_SCRIPT_CMA_ISLAND_MULTI --dimension 100 --function \"$func\" --active_cma true --pollination false" > "$DIRECTORY/multi_dim/cma_island/active/migr/multi100_cma_active_migr_${func}_run${run}.sh"
-
-    # Submit jobs and redirect stdout to a separate output file for each job
-    sbatch "$DIRECTORY/default_prop/migr/default_prop_migr_${func}_run${run}.sh" > "$DIRECTORY/default_prop/migr/default_prop_migr_${func}_run${run}.out"
-    sbatch "$DIRECTORY/default_prop/polli/default_prop_polli_${func}_run${run}.sh" > "$DIRECTORY/default_prop/polli/default_prop_polli_${func}_run${run}.out"
-    sbatch "$DIRECTORY/cma/basic/cma_basic_${func}_run${run}.sh" > "$DIRECTORY/cma/basic/cma_basic_${func}_run${run}.out"
-    sbatch "$DIRECTORY/cma/active/cma_active_${func}_run${run}.sh" > "$DIRECTORY/cma/active/cma_active_${func}_run${run}.out"
-    sbatch "$DIRECTORY/cma_island/basic/migr/cma_basic_migr_${func}_run${run}.sh" > "$DIRECTORY/cma_island/basic/migr/cma_basic_migr_${func}_run${run}.out"
-    #sbatch "$DIRECTORY/cma_island/basic/polli/cma_basic_polli_${func}.sh" > "$DIRECTORY/cma_island/basic/polli/cma_basic_polli_${func}_run${run}.out"
-    sbatch "$DIRECTORY/cma_island/active/migr/cma_active_migr_${func}_run${run}.sh" > "$DIRECTORY/cma_island/active/migr/cma_active_migr_${func}_run${run}.out"
-    #sbatch "$DIRECTORY/cma_island/active/polli/cma_active_polli_${func}.sh" > "$DIRECTORY/cma_island/active/polli/cma_active_polli_${func}_run${run}.out"
-
-    sbatch "$DIRECTORY/multi_dim/default_prop/polli/multi_10_default_prop_polli_${func}_run${run}.sh" > "$DIRECTORY/multi_dim/default_prop/polli/multi_10_default_prop_polli_${func}_run${run}.out"
-    sbatch "$DIRECTORY/multi_dim/default_prop/polli/multi_100_default_prop_polli_${func}_run${run}.sh" > "$DIRECTORY/multi_dim/default_prop/polli/multi_100_default_prop_polli_${func}_run${run}.out"
-    sbatch "$DIRECTORY/multi_dim/cma/active/multi10_cma_active_${func}_run${run}.sh" > "$DIRECTORY/multi_dim/cma/active/multi10_cma_active_${func}_run${run}.out"
-    sbatch "$DIRECTORY/multi_dim/cma/active/multi100_cma_active_${func}_run${run}.sh" > "$DIRECTORY/multi_dim/cma/active/multi100_cma_active_${func}_run${run}.out"
-    sbatch "$DIRECTORY/multi_dim/cma_island/active/migr/multi10_cma_active_migr_${func}_run${run}.sh" > "$DIRECTORY/multi_dim/cma_island/active/migr/multi10_cma_active_migr_${func}_run${run}.out"
-    sbatch "$DIRECTORY/multi_dim/cma_island/active/migr/multi100_cma_active_migr_${func}_run${run}.sh" > "$DIRECTORY/multi_dim/cma_island/active/migr/multi100_cma_active_migr_${func}_run${run}.out"
-  done
-done
