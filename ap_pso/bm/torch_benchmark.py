8f9c03ab697ecb0e51461bc12b721011c991c97e
Morridin <paulza@gmx.de>
2023-08-25 18:58:54 +0200

    Added the NAS benchmark setup.

---
+++ ap_pso/bm/torch_benchmark.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+
+import random
+import sys
+import time
+
+import numpy as np
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+
+from pytorch_lightning import LightningModule, Trainer
+from torchmetrics import Accuracy
+
+from torchvision.datasets import MNIST
+from torchvision.transforms import Compose, ToTensor, Normalize
+
+from mpi4py import MPI
+
+from ap_pso.propagators import *
+from propulate import Islands
+from propulate.propagators import Conditional
+
+num_generations = 3
+pop_size = 2 * MPI.COMM_WORLD.size
+GPUS_PER_NODE = 4
+
+limits = {
+    "convlayers": (2.0, 10.0),
+    "lr": (0.01, 0.0001),
+}
+
+
+class Net(LightningModule):
+    def __init__(self, convlayers: int, activation, lr: float, loss_fn):
+        super(Net, self).__init__()
+
+        self.best_accuracy: float = 0.0
+        self.lr = lr
+        self.loss_fn = loss_fn
+        layers = []
+        layers += [
+            nn.Sequential(nn.Conv2d(1,
+                                    10,
+                                    kernel_size=3,
+                                    padding=1),
+                          activation()),
+        ]
+        layers += [
+            nn.Sequential(nn.Conv2d(10,
+                                    10,
+                                    kernel_size=3,
+                                    padding=1),
+                          activation())
+            for _ in range(convlayers - 1)
+        ]
+
+        self.fc = nn.Linear(7840, 10)
+        self.conv_layers = nn.Sequential(*layers)
+
+        self.val_acc = Accuracy('multiclass', num_classes=10)
+
+    def forward(self, x):
+        b, c, w, h = x.size()
+        x = self.conv_layers(x)
+        x = x.view(b, 10 * 28 * 28)
+        x = self.fc(x)
+
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        loss = self.loss_fn(self(x), y)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        pred = self(x)
+        loss = self.loss_fn(pred, y)
+        val_acc = self.val_acc(torch.nn.functional.softmax(pred, dim=-1), y)
+        if val_acc > self.best_accuracy:
+            self.best_accuracy = val_acc
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.parameters(), lr=self.lr)
+        return optimizer
+
+
+def get_data_loaders(batch_size, dl=False):
+    data_transform = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
+    train_loader = DataLoader(
+        MNIST(download=dl, root=".", transform=data_transform, train=True),
+        batch_size=batch_size,
+        shuffle=True,
+    )
+    val_loader = DataLoader(
+        MNIST(download=dl, root=".", transform=data_transform, train=False),
+        batch_size=1,
+        shuffle=False,
+    )
+    return train_loader, val_loader
+
+
+def ind_loss(params):
+    convlayers = int(np.round(params["convlayers"]))
+    activation = "leakyrelu"
+    lr = params["lr"]
+    epochs = 2
+
+    activations = {"relu": nn.ReLU, "sigmoid": nn.Sigmoid, "tanh": nn.Tanh, "leakyrelu": nn.LeakyReLU}
+    activation = activations[activation]
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+    model = Net(convlayers, activation, lr, loss_fn)
+    if MPI.COMM_WORLD.rank == 0:
+        train_loader, val_loader = get_data_loaders(8, True)
+    MPI.COMM_WORLD.barrier()
+    if MPI.COMM_WORLD.rank != 0:
+        train_loader, val_loader = get_data_loaders(8)
+    trainer = Trainer(max_epochs=epochs,
+                      accelerator='gpu',
+                      devices=[
+                          MPI.COMM_WORLD.Get_rank() % GPUS_PER_NODE
+                      ],
+                      enable_progress_bar=False,
+                      )
+    trainer.fit(model, train_loader, val_loader)
+
+    return -model.best_accuracy
+
+
+if __name__ == "__main__":
+    rng = random.Random(MPI.COMM_WORLD.rank)
+    pso = [
+        VelocityClampingPropagator(0.7298, 1.49618, 1.49618, MPI.COMM_WORLD.rank, limits, rng, 0.6),
+        ConstrictionPropagator(2.49618, 2.49618, MPI.COMM_WORLD.rank, limits, rng),
+        BasicPSOPropagator(0.7298, 0.5, 0.5, MPI.COMM_WORLD.rank, limits, rng),
+        CanonicalPropagator(2.49618, 2.49618, MPI.COMM_WORLD.rank, limits, rng)
+    ][int(sys.argv[1])]
+
+    # propagator = get_default_propagator(pop_size, limits, 0.7, 0.4, 0.1, rng=rng)
+
+    if MPI.COMM_WORLD.rank == 0:
+        print("#-----------------------------------#")
+        print(f"| Current time: {time.time_ns()} |")
+        print("#-----------------------------------#")
+
+    propagator = Conditional(pop_size, pso, PSOInitUniform(limits, rng=rng, rank=MPI.COMM_WORLD.rank))
+    islands = Islands(ind_loss, propagator, rng, generations=num_generations, num_islands=1, migration_probability=0)
+    islands.evolve(top_n=1, debug=2)
+
+    if MPI.COMM_WORLD.rank == 0:
+        print("#-----------------------------------#")
+        print(f"| Current time: {time.time_ns()} |")
+        print("#-----------------------------------#")
