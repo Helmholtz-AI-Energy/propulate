122d1e093188f1511e67e3adde5fccbeb1ec4236
Morridin <paulza@gmx.de>
2023-09-04 14:26:30 +0200

    Merge into Propulate completed.

--- ap_pso/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This package contains - except for the example and the init propagator everything I added to propulate to be able to
-run PSO on it.
-"""
-__all__ = ["Particle", "propagators", "make_particle"]
-
-from propulate.particle import Particle
-from ap_pso.utils import make_particle
