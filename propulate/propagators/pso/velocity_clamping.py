d175f37dc03fd8f17cb15e998be8b2d064f05b80
Morridin <paulza@gmx.de>
2023-09-21 11:57:32 +0200

    Reworked the doc strings.

--- propulate/propagators/pso/velocity_clamping.py
+++ propulate/propagators/pso/velocity_clamping.py
@@ -33,15 +33,28 @@ class VelocityClamping(Basic):
         v_limits: Union[float, np.ndarray],
     ):
         """
-        Class constructor.
-        :param inertia: The particle's inertia factor
-        :param c_cognitive: constant cognitive factor to scale p_best with
-        :param c_social: constant social factor to scale g_best with
-        :param rank: the rank of the worker the propagator is living on in MPI.COMM_WORLD
-        :param limits: a dict with str keys and 2-tuples of floats associated to each of them
-        :param rng: random number generator :param v_limits: a numpy array containing values that work as relative caps
-                    for their corresponding search space dimensions.
-                    If this is a float instead, it does its job for all axes.
+        The class constructor.
+
+        Parameters
+        ----------
+        inertia : float
+                  The particle's inertia factor
+        c_cognitive : float
+                      Constant cognitive factor to scale the distance to the particle's personal best value with
+        c_social : float
+                   Constant social factor to scale the distance to the swarm's global best value with
+        rank : int
+               The global rank of the worker the propagator is living on
+        limits : Dict[str, Tuple[float, float]]
+                 A dict with str keys and 2-tuples of floats associated to each of them. It describes the borders of
+                 the search domain.
+        rng : random.Random
+              Random number generator for said non-linearity
+        v_limits : Union[float, np.ndarray]
+                   This parameter is multiplied with the clamping limit in order to reduce it further in most cases
+                   (this is, when the value is in (0; 1)).
+                   If this parameter has float type, it is applied to all dimensions of the search domain, else,
+                   each of its elements are applied to their corresponding dimension of the search domain.
         """
         super().__init__(inertia, c_cognitive, c_social, rank, limits, rng)
         x_min, x_max = self.limits_as_array
@@ -51,6 +64,25 @@ class VelocityClamping(Basic):
         self.v_cap: np.ndarray = np.array([-v_limits * x_range, v_limits * x_range])
 
     def __call__(self, individuals: List[Individual]) -> Particle:
+        """
+        Applies the standard PSO update rule with inertia, extended by cutting off too high velocities.
+
+        Returns a Particle object that contains the updated values of the youngest passed Particle or Individual that
+        belongs to the worker the propagator is living on.
+
+        Parameters
+        ----------
+        individuals: List[Individual]
+                     A list of individuals that must at least contain one individual that belongs to the propagator.
+                     This list is used to calculate personal and global best of the particle and the swarm and to
+                     then update the particle based on the retrieved results.
+                     Individuals that cannot be used as Particle class objects are copied to particles before going on.
+
+        Returns
+        -------
+        propulate.population.Particle
+            An updated Particle.
+        """
         old_p, p_best, g_best = self._prepare_data(individuals)
 
         new_velocity: np.ndarray = (
