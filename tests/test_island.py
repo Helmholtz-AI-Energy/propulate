3e9c8f26eae0084e441d5ee1de61abeefb7626d9
Oskar Taubert <oskar.taubert@kit.edu>
2023-10-30 10:30:14 +0100

    set up stubs for all the new tests needed

---
+++ tests/test_island.py
@@ -0,0 +1,27 @@
+def test_Island():
+    # TODO test run
+    raise
+
+def test_checkpointing_isolated():
+    # do just parallel island without migration for easier debugging
+    raise
+
+
+def test_checkpointing():
+    # TODO basic checkpointing test with islands with migration/pollination
+    raise
+
+
+def test_checkpointing_unequal_populations():
+    # TODO test loading unequal islands
+    raise
+
+
+def test_checkpointing_setup_downgrade():
+    # TODO write checkpoint with large setup and read it with a small one
+    raise
+
+
+def test_checkpointing_setup_upgrade():
+    # TOOD write checkpoint with small setup and read it with a large one
+    raise
--- tests/test_propulator.py
+++ tests/test_propulator.py
@@ -2,6 +2,7 @@ import random
 import tempfile
 from operator import attrgetter
 import logging
+from copy import deepcopy
 
 from propulate import Propulator
 from propulate.utils import get_default_propagator, set_logger_config, sphere
@@ -49,3 +50,38 @@ def test_Propulator():
         best = min(propulator.population, key=attrgetter("loss"))
 
         assert best.loss < 0.8
+
+
+def test_checkpointing_propulator():
+    rng = random.Random(42)  # Separate random number generator for optimization.
+    limits = {
+        "a": (-5.12, 5.12),
+        "b": (-5.12, 5.12),
+    }
+
+    with tempfile.TemporaryDirectory() as checkpoint_directory:
+        propagator = get_default_propagator(limits=limits, rng=rng)
+        propulator = Propulator(
+            loss_fn=sphere,
+            propagator=propagator,
+            generations=10,
+            checkpoint_directory=checkpoint_directory,
+            rng=rng,
+        )
+
+        propulator.propulate()
+
+        population = deepcopy(propulator.population)
+
+        del propulator
+
+        propulator = Propulator(
+            loss_fn=sphere,
+            propagator=propagator,
+            generations=20,
+            checkpoint_directory=checkpoint_directory,
+            rng=rng,
+        )
+
+        # TODO properly check the loaded from checkpoint  population is the same as the one retained from the first run
+        assert population == propulator.population
