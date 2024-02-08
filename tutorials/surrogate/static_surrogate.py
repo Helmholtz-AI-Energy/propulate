6350f89b0dd3e7c6fcf4be73ae33b8170a390053
vtotiv <vito@dierksen.me>
2024-02-08 16:50:49 +0100

    Simplified StaticSurrogate
    added logging prints for each method call
    and implemented a start_run which directly initializes the current_run
    array to the full size, preventing runtim resizes

--- tutorials/surrogate/static_surrogate.py
+++ tutorials/surrogate/static_surrogate.py
@@ -1,18 +1,21 @@
-from propulate import Surrogate
 import numpy as np
+from propulate import Surrogate
+from propulate.population import Individual
 
 
 class StaticSurrogate(Surrogate):
     """
     Surrogate model using the best known run as baseline.
-    After the first run, each subsequent onw is compared to the baseline.
-    Every run outside a margin of the baseline is cancelled.
+    After the first run, each subsequent loss is compared to the baseline.
+    Every run with a loss outside the margin of the baseline is cancelled.
 
     This model assumes regular yields between training runs,
     otherwise the indices of the baseline run won't match.
     """
 
     def __init__(self, margin: float = 0.8):
+        print("Static Surrogate - init")
+
         super().__init__()
 
         self.synthetic_id: int = 0
@@ -26,45 +29,61 @@ class StaticSurrogate(Surrogate):
         self.baseline: np.ndarray = np.zeros((0), dtype=float)
         self.current_run: np.ndarray = np.zeros((0), dtype=float)
 
-    def update(self, loss: float):
-        if self.first_run:
-            self.baseline = self.current_run.copy()
-            self.first_run = False
-
-        # check if current run is better than baseline
-        # current run is initialized with zeros, not finished runs are ignored
-        if self.baseline[-1] < self.current_run[-1]:
-            self.baseline = self.current_run.copy()
+    def start_run(self, ind: Individual):
+        print("Static Surrogate - start run - ind", ind.keys(), ind.values())
 
         # reset to new run
         self.synthetic_id = 0
-
+        # reset current run with correct size
         self.current_run = np.zeros((self.baseline.size), dtype=float)
 
+    def update(self, loss: float):
+        print("Static Surrogate - update - loss", loss)
+
+        if self.first_run:
+            self.baseline = self.current_run.copy()
+            self.first_run = False
+            return
+
+        # check if current run is better than baseline
+        if self.baseline[-1] > self.current_run[-1]:
+            self.baseline = self.current_run.copy()
+
     def cancel(self, loss: float) -> bool:
+        print("Static Surrogate - cancel - loss", loss)
+
         self.synthetic_id += 1
 
         # cancel is only allowed after the first complete run
         if self.first_run:
+            self.current_run = np.append(self.current_run, loss)
             return False
 
+        # append loss to current run
+        self.current_run[self.synthetic_id - 1] = loss
+
+        print("Static Surrogate - cancel - compare with baseline", self.baseline[self.synthetic_id - 1], "and loss", loss * self.margin)
         # cancel if current run is outside margin of baseline
-        if self.baseline[self.synthetic_id - 1] * self.margin > loss:
+        if self.baseline[self.synthetic_id - 1] < loss * self.margin:
             return True
 
-        self.current_run[self.synthetic_id - 1] = loss
         return False
 
     def merge(self, data: np.ndarray):
+        print("Static Surrogate - merge")
+
+        # no prior data to merge with
         if self.first_run:
             self.baseline = data.copy()
             self.first_run = False
             return
 
         # merged runs final loss is better than baseline
-        if self.baseline[-1][1] < data[-1][1]:
+        if self.baseline[-1] < data[-1]:
             self.baseline = data.copy()
 
     def data(self) -> np.ndarray:
+        print("Static Surrogate - data")
+
         # return best run so far
         return self.baseline
