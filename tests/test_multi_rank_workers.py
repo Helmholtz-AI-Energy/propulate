0640ba6d6334a820b8da10837dccee0aa6bead9b
Marie Weiel <marie.weiel@kit.edu>
2024-04-23 14:20:17 +0200

    clean up file

--- tests/test_multi_rank_workers.py
+++ tests/test_multi_rank_workers.py
@@ -1,3 +1,4 @@
+import pathlib
 import random
 from typing import Dict
 
@@ -6,7 +7,6 @@ import pytest
 from mpi4py import MPI
 
 from propulate import Islands
-from propulate.propagators import SelectMax, SelectMin
 from propulate.utils import get_default_propagator, set_logger_config
 
 
@@ -41,40 +41,37 @@ def parallel_sphere(params: Dict[str, float], comm: MPI.Comm = MPI.COMM_SELF) ->
 
 
 @pytest.mark.mpi(min_size=8)
-def test_multi_rank_workers(mpi_tmp_path):
-    """Test multi rank workers. Two Islands with at least two workers with two ranks each."""
+def test_multi_rank_workers(mpi_tmp_path: pathlib.Path) -> None:
+    """
+    Test multi-rank workers. Two islands with at least two workers with two ranks each.
+
+    Parameters
+    ----------
+    mpi_tmp_path : pathlib.Path
+        The temporary checkpoint directory.
+    """
     full_world_comm = MPI.COMM_WORLD  # Get full world communicator.
-    set_logger_config()
+    set_logger_config(log_file=mpi_tmp_path / "log.log")
 
     rng = random.Random(42 + full_world_comm.rank)
     limits = {
         "a": (-5.12, 5.12),
         "b": (-5.12, 5.12),
     }
-    function = parallel_sphere
     propagator = get_default_propagator(  # Get default evolutionary operator.
         pop_size=2,  # Breeding pool size
         limits=limits,  # Search-space limits
         rng=rng,  # Separate random number generator for Propulate optimization
     )
 
-    # Set up migration topology.
-    migration_topology = 1 * np.ones((2, 2), dtype=int)
-    np.fill_diagonal(
-        migration_topology, 0
-    )  # An island does not send migrants to itself.
-
     # Set up island model.
     islands = Islands(
-        loss_fn=function,  # Loss function to be minimized
+        loss_fn=parallel_sphere,  # Loss function to be minimized
         propagator=propagator,  # Propagator, i.e., evolutionary operator to be used
         rng=rng,  # Separate random number generator for Propulate optimization
         generations=100,  # Overall number of generations
         num_islands=2,  # Number of islands
-        migration_topology=migration_topology,  # Migration topology
         migration_probability=0.9,  # Migration probability
-        emigration_propagator=SelectMin,  # Selection policy for emigrants
-        immigration_propagator=SelectMax,  # Selection policy for immigrants
         pollination=False,  # Whether to use pollination or migration
         checkpoint_path=mpi_tmp_path,  # Checkpoint path
         # ----- SPECIFIC FOR MULTI-RANK UCS ----
