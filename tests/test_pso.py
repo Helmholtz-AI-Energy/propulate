3e5d73e30b96c9c06c89d6b08d6aac4f5cd3d3a0
Marie Weiel <marie.weiel@kit.edu>
2024-04-30 15:03:31 +0200

    add fixture to test all PSO propagators

--- tests/test_pso.py
+++ tests/test_pso.py
@@ -1,68 +1,80 @@
 import pathlib
 import random
-from typing import Tuple
 
 import pytest
 from mpi4py import MPI
 
 from propulate import Propulator
 from propulate.propagators import Conditional
-from propulate.propagators.pso import BasicPSO, InitUniformPSO
-from propulate.utils.benchmark_functions import get_function_search_space
+from propulate.propagators.pso import (
+    BasicPSO,
+    InitUniformPSO,
+    VelocityClampingPSO,
+    CanonicalPSO,
+    ConstrictionPSO,
+)
+from propulate.utils.benchmark_functions import get_function_search_space, sphere
+
+limits = get_function_search_space("sphere")[1]
+rank = MPI.COMM_WORLD.rank
+rng = random.Random(42 + rank)
 
 
 @pytest.fixture(
     params=[
-        ("rosenbrock", 0.0),
-        ("step", -25.0),
-        ("quartic", 0.0),
-        ("rastrigin", 0.0),
-        ("griewank", 0.0),
-        ("schwefel", 0.0),
-        ("bisphere", 0.0),
-        ("birastrigin", 0.0),
-        ("bukin", 0.0),
-        ("eggcrate", -1.0),
-        ("himmelblau", 0.0),
-        ("keane", 0.6736675),
-        ("leon", 0.0),
-        ("sphere", 0.0),  # (fname, expected)
+        BasicPSO(
+            inertia=0.729,
+            c_cognitive=1.49445,
+            c_social=1.49445,
+            rank=rank,
+            limits=limits,
+            rng=rng,
+        ),
+        VelocityClampingPSO(
+            inertia=0.729,
+            c_cognitive=1.49445,
+            c_social=1.49445,
+            rank=rank,
+            limits=limits,
+            rng=rng,
+            v_limits=0.6,
+        ),
+        ConstrictionPSO(
+            c_cognitive=2.05,
+            c_social=2.05,
+            rank=rank,
+            limits=limits,
+            rng=rng,
+        ),
+        CanonicalPSO(
+            c_cognitive=2.05, c_social=2.05, rank=rank, limits=limits, rng=rng
+        ),
     ]
 )
-def function_parameters(request):
-    """Define benchmark function parameter sets as used in tests."""
+def pso_propagator(request):
+    """Iterate over PSO propagator variants."""
     return request.param
 
 
 @pytest.mark.mpi
-def test_pso(function_parameters: Tuple[str, float, float], mpi_tmp_path: pathlib.Path):
+def test_pso(pso_propagator, mpi_tmp_path: pathlib.Path):
     """
     Test single worker using Propulator to optimize a benchmark function using the default genetic propagator.
 
     Parameters
     ----------
-    function_parameters : Tuple
-        The tuple containing each function name along with its global minimum.
+    pso_propagator : BasicPSO
+        The PSO propagator variant to test.
     mpi_tmp_path : pathlib.Path
         The temporary checkpoint directory.
     """
-    rng = random.Random(42)  # Separate random number generator for optimization.
-    function, limits = get_function_search_space(function_parameters[0])
     # Set up evolutionary operator.
-    pso_propagator = BasicPSO(
-        inertia=0.729,
-        c_cognitive=1.49334,
-        c_social=1.49445,
-        rank=MPI.COMM_WORLD.rank,  # MPI rank
-        limits=limits,
-        rng=rng,
-    )
-    init = InitUniformPSO(limits, rng=rng, rank=MPI.COMM_WORLD.rank)
+    init = InitUniformPSO(limits, rng=rng, rank=rank)
     propagator = Conditional(1, pso_propagator, init)
 
     # Set up propulator performing actual optimization.
     propulator = Propulator(
-        loss_fn=function,
+        loss_fn=sphere,
         propagator=propagator,
         rng=rng,
         generations=100,
