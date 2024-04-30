e5c762fceed7805519e946ff2ce5d5c7f225d8a0
Marie Weiel <marie.weiel@kit.edu>
2024-04-30 14:31:54 +0200

    remove deprecated pkg_resources in favor of importlib.metadata

--- propulate/__init__.py
+++ propulate/__init__.py
@@ -1,14 +1,12 @@
-# -*- coding: utf-8 -*-
-from pkg_resources import DistributionNotFound, get_distribution
+from importlib.metadata import PackageNotFoundError, version
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
+    __version__ = version(__name__)
+except PackageNotFoundError:
     __version__ = "unknown"
 finally:
-    del get_distribution, DistributionNotFound
+    del version, PackageNotFoundError
 
 from . import propagators
 from .islands import Islands
