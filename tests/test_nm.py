6c98feae1051ffaee9f8a1041b6918849e91ab4f
Oskar Taubert <oskar.taubert@kit.edu>
2024-05-08 01:42:00 +0200

    added example and integration test for simplex optimizer

---
+++ tests/test_nm.py
@@ -0,0 +1,68 @@
+import pathlib
+import random
+from typing import Tuple
+
+import numpy as np
+import pytest
+
+from propulate import Propulator
+from propulate.propagators import ParallelNelderMead
+from propulate.utils.benchmark_functions import get_function_search_space
+
+
+@pytest.fixture(
+    params=[
+        ("rosenbrock", 0.0),
+        ("step", -25.0),
+        ("quartic", 0.0),
+        ("rastrigin", 0.0),
+        ("griewank", 0.0),
+        ("schwefel", 0.0),
+        ("bisphere", 0.0),
+        ("birastrigin", 0.0),
+        ("bukin", 0.0),
+        ("eggcrate", -1.0),
+        ("himmelblau", 0.0),
+        ("keane", 0.6736675),
+        ("leon", 0.0),
+        ("sphere", 0.0),  # (fname, expected)
+    ]
+)
+def function_parameters(request):
+    """Define benchmark function parameter sets as used in tests."""
+    return request.param
+
+
+def test_cmaes(
+    function_parameters: Tuple[str, float], mpi_tmp_path: pathlib.Path
+) -> None:
+    """
+    Test Propulator to optimize a benchmark function using a Nelder-Mead propagator.
+
+    This test is run both sequentially and in parallel.
+
+    Parameters
+    ----------
+    function_parameters : Tuple[str, float]
+        The tuple containing each function name along with its global minimum.
+    mpi_tmp_path : pathlib.Path
+        The temporary checkpoint directory.
+    """
+    rng = random.Random(42)  # Separate random number generator for optimization.
+    function, limits = get_function_search_space(function_parameters[0])
+    # Set up evolutionary operator.
+    low = np.array([v[0] for v in limits.values()])
+    high = np.array([v[1] for v in limits.values()])
+    start_point = np.random.default_rng(seed=235231).uniform(low=low, high=high)
+    propagator = ParallelNelderMead(limits, rng=rng, start=start_point)
+
+    # Set up Propulator performing actual optimization.
+    propulator = Propulator(
+        loss_fn=function,
+        propagator=propagator,
+        rng=rng,
+        generations=100,
+        checkpoint_path=mpi_tmp_path,
+    )
+    # Run optimization and print summary of results.
+    propulator.propulate()
---
+++ tutorials/nm_example.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import pathlib
+import random
+
+import numpy as np
+from mpi4py import MPI
+
+from propulate import Propulator
+from propulate.propagators.nm import AdaptedNM
+from propulate.utils import set_logger_config
+from propulate.utils.benchmark_functions import (
+    get_function_search_space,
+    parse_arguments,
+)
+
+if __name__ == "__main__":
+    comm = MPI.COMM_WORLD
+
+    if comm.rank == 0:
+        print(
+            "#################################################\n"
+            "# PROPULATE: Parallel Propagator of Populations #\n"
+            "#################################################\n"
+        )
+
+    # Parse command-line arguments.
+    config, _ = parse_arguments(comm)
+
+    # Set up separate logger for Propulate optimization.
+    set_logger_config(
+        level=config.logging_level,  # logging level
+        log_file=f"{config.checkpoint}/{pathlib.Path(__file__).stem}.log",  # Logging path
+        log_to_stdout=True,  # Print log on stdout.
+        log_rank=False,  # Do not prepend MPI rank to logging messages.
+        colors=True,  # Use colors.
+    )
+
+    rng = random.Random(
+        config.seed + comm.rank
+    )  # Separate random number generator for optimization.
+    function, limits = get_function_search_space(
+        config.function
+    )  # Get callable function + search-space limits.
+
+    # randomly choose a start point from within the limits
+    low = np.array([v[0] for v in limits.values()])
+    high = np.array([v[1] for v in limits.values()])
+    start_point = np.random.default_rng(seed=config.seed + 235231).uniform(
+        low=low, high=high
+    )
+    propagator = AdaptedNM(limits, rng=rng, start=start_point)
+    # Set up propulator performing actual optimization.
+    propulator = Propulator(
+        loss_fn=function,
+        propagator=propagator,
+        comm=comm,
+        generations=config.generations,
+        checkpoint_directory=config.checkpoint,
+        rng=rng,
+    )
+
+    # Run optimization and print summary of results.
+    propulator.propulate(logging_interval=config.logging_int, debug=config.verbosity)
+    propulator.summarize(top_n=config.top_n, debug=config.verbosity)
