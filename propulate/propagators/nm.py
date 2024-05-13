37ca0960cb795c34c954a30adab43ab90530f860
Marie Weiel <marie.weiel@kit.edu>
2024-05-13 11:54:38 +0200

    fix docstrings and type hints

--- propulate/propagators/nm.py
+++ propulate/propagators/nm.py
@@ -9,24 +9,25 @@ from .base import Gaussian, Propagator, SelectMin
 
 class ParallelNelderMead(Propagator):
     """
-    Parallel Nelder Mead propagator.
+    Parallel Nelder-Mead propagator.
 
     Attributes
     ----------
     limits : Dict[str, Tuple[float, float]] | Dict[str, Tuple[int, int]] | Dict[str, Tuple[str, ...]]
         The search space, i.e., the limits of (hyper-)parameters to be optimized.
-    alpha: float
-        reflection parameter
+    alpha : float
+        The reflection parameter.
     gamma : float
-        expansion parameter
+        The expansion parameter.
     rho : float
-        contraction parameter
+        The contraction parameter.
     sigma : float
-        shrinking parameter
+        The shrinking parameter.
     generation : int
-        Current optimization iteration. Not the same as a normal Nelder Mead iteration, since there can only be a single loss evaluation per iteration in the adapted algorithm.
+        The current optimization iteration. Not the same as a normal Nelder-Mead iteration,
+        since there can only be a single loss evaluation per iteration in the adapted algorithm.
     simplex : list
-        List of Individual instances forming the current simplex.
+        The list of ``Individual`` instances forming the current simplex.
 
     """
 
@@ -42,7 +43,7 @@ class ParallelNelderMead(Propagator):
         scale: float = 1.0,
     ):
         """
-        Initialize Nelder-Mead propagator.
+        Initialize a Nelder-Mead propagator.
 
         Parameters
         ----------
@@ -51,18 +52,17 @@ class ParallelNelderMead(Propagator):
         rng : random.Random, optional
             The separate random number generator of the Propulate optimization.
         start : numpy.ndarray
-            Starting point around which the first simplex is constructed.
+            The starting point around which the first simplex is constructed.
         alpha : float, optional
-            Reflection parameter
+            The reflection parameter.
         gamma : float, optional
-            Expansion parameter
+            The expansion parameter.
         rho : float, optional
-            Contraction parameter
+            The contraction parameter.
         sigma : float, optional
-            Shrinking parameter
-        scale: float, optional
-            Size of the initial simplex.
-
+            The shrinking parameter.
+        scale : float, optional
+            The size of the initial simplex.
         """
         super().__init__(parents=-1, offspring=1, rng=rng)
         self.limits = limits
@@ -97,7 +97,6 @@ class ParallelNelderMead(Propagator):
         -------
         propulate.Individual
             The individual after application of the propagator.
-
         """
         if len(inds) < self.problem_dimension + 1:
             ind = self.init([Individual(self.start, self.limits)])
@@ -141,82 +140,85 @@ class ParallelNelderMead(Propagator):
         self.generation += 1
         return ind
 
-    def compute_centroid(self):
+    def compute_centroid(self) -> np.ndarray:
         """
         Compute the centroid of the current simplex.
 
         Returns
         -------
         numpy.ndarray
-            Centroid of current simplex.
+            The centroid of current simplex.
         """
         position = sum([x.position for x in self.simplex[:-1]]) / (
             len(self.simplex) - 1
         )
         return position
 
-    def reflect(self):
+    def reflect(self) -> Individual:
         """
         Nelder-Mead reflect step.
 
         Returns
         -------
-        numpy.ndarray
-            Returns the reflection of the worst point in the simplex on the centroid.
+        Individual
+            The reflection of the worst point in the simplex on the centroid.
         """
         centroid = self.compute_centroid()
         position = centroid + self.alpha * (centroid - self.simplex[-1].position)
 
         return Individual(position, self.limits)
 
-    def expand(self):
+    def expand(self) -> Individual:
         """
         Nelder-Mead expand step.
 
         Returns
         -------
-        numpy.ndarray
-            Returns the reflection of the worst point in the simplex on the centroid with a larger step size.
+        Individual
+            The reflection of the worst point in the simplex on the centroid with a larger step size.
         """
         centroid = self.compute_centroid()
         position = centroid + self.gamma * (self.simplex[0].position - centroid)
         return Individual(position, self.limits)
 
-    def outercontract(self):
+    def outercontract(self) -> Individual:
         """
         Nelder-Mead contract step on the outside of the simplex.
 
         Returns
         -------
-        numpy.ndarray
-            Returns
+        Individual
+            The individual contracted on the outside of the simplex.
         """
         centroid = self.compute_centroid()
         position = centroid + self.rho * (self.simplex[-1].position - centroid)
         return Individual(position, self.limits)
 
-    def innercontract(self):
+    def innercontract(self) -> Individual:
         """
         Nelder-Mead contract step on the inside of the simplex.
 
         Returns
         -------
-        numpy.ndarray
+        Individual
+            The individual contracted on the inside of the simplex.
         """
-        # TODO find a way to make the distinction whether to expect a better point on the outside or the inside of the simplex
-        # for now assume inside, if not the next generation should be a reflection, probably not the most efficient
+        # TODO Find a way to make the distinction whether to expect a better point on the outside or the inside of the
+        #  simplex. For now assume inside, if not the next generation should be a reflection, probably not the most
+        #  efficient.
         centroid = self.compute_centroid()
         position = centroid + self.rho * (self.simplex[-1].position - centroid)
 
         return Individual(position, self.limits)
 
-    def shrink(self):
+    def shrink(self) -> Individual:
         """
         Nelder-Mead shrink step only touching a single random vertex of the current simplex.
 
         Returns
         -------
-        numpy.ndarray
+        Individual
+            The shrunk individual.
         """
         i = self.rng.randrange(1, len(self.simplex))
 
